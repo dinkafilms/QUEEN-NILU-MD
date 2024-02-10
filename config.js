2531ace22d93a4cae1bcb9a44bfbfd75104d78c7
dinkafilms <159150600+dinkafilms@users.noreply.github.com>
2024-02-10 10:30:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,18 +14,18 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = '' // QUEENNILU;;;wvsiBBIY#F3qHNGCAUumgdEUFT24WlLAOtCd6wyQgISZLgdl8wrc
 
 // databse URL  ##Required 
-global.POSTGRESQL_URL = ''
+global.POSTGRESQL_URL = '' postgres://nadeen_user:7LdcnxSYDM3LHRrtWa4DrtjO67nkHXPN@dpg-cn31b07109ks73em4mp0-a/nadeen
 
  
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '94716769285'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'NAdeen'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
@@ -47,7 +47,7 @@ global.FOOTER = 'Queen Nilu 2023'
 
 global.BOT_OFFLINE = false    //  Always Onlne -  | false offline -True
 
-global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
+global.STICKER_NAME = '@Nadeen🤹‍♂️' //sticker
 
 global.LANG = 'EN' // Language (EN-  TO english  ,  SI- TO sinhala )
 
